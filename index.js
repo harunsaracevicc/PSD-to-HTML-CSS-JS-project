1c557cc4adeda6bd23a03773718484f32ca9c6af
Harun Saračević <harunsaracevicc@gmail.com>
2022-02-07 21:13:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function MobileMenu() {
+  var x = document.getElementById("mobilenav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
